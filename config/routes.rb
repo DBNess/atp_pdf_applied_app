3c9c0f156bee93e0e9ba82e4ea8072e2d287bad1
Vanessa Hurst <vrhurst@gmail.com>
2012-06-10 22:38:58 -0400

    add basic header with twitter auth status

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,11 @@ Platform::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
-  #devise_scope :user do
-  #  get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
-  #  get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
-  #end
-  
   devise_scope :user do
-    delete "/logout" => "devise/sessions#destroy"
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
-
+  
   get "home/index"
   root :to => "home#index"
 
